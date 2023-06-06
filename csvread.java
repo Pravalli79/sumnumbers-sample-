9c87bd13b1aa063d69ea94b486e8bc0440eca94f
Pravalli79 <122410117+Pravalli79@users.noreply.github.com>
2023-06-06 10:59:28 -0500

    Add files via upload

---
+++ csvread.java
@@ -0,0 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class csvread {
+
+public static void main(String[] args) throws IOException
+{
+File f = new File("/Users/phanijayasreeirrinki/my_prediction.csv");
+FileWriter fr = new FileWriter(f,true);
+BufferedWriter br =  new BufferedWriter(fr);
+for(int i= 1; i<10;i++)
+{
+    br.write("RCV Academy online"+",");
+}
+System.out.println(" Written successfully ");
+br.close();
+
+}
+}
